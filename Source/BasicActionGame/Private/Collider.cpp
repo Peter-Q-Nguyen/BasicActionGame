18006b5f796548fa86bc1eafcd0f9111843872d4
Peter Nguyen <spacechannel6@gmail.com>
2020-07-06 18:09:08 +0100

    initial commit adding c++ code for setting up an Actor with a camera

---
+++ Source/BasicActionGame/Private/Collider.cpp
@@ -0,0 +1,82 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Collider.h"
+#include "Components/SphereComponent.h"
+#include "Components/InputComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
+// Sets default values
+ACollider::ACollider()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+
+	SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComponent"));
+	SphereComponent->SetupAttachment(GetRootComponent());
+	SphereComponent->InitSphereRadius(40.0f);
+	SphereComponent->SetCollisionProfileName(TEXT("Pawn"));
+
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComponent"));
+	MeshComponent->SetupAttachment(GetRootComponent());
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> MeshComponentAsset(TEXT("StaticMesh'/Game/StarterContent/Shapes/Shape_Sphere.Shape_Sphere'"));
+	if (MeshComponentAsset.Succeeded())
+	{
+		MeshComponent->SetStaticMesh(MeshComponentAsset.Object);
+		MeshComponent->SetRelativeLocation(FVector(0.f, 0.f, -40.f));
+		MeshComponent->SetWorldScale3D(FVector(0.8f, 0.8f, 0.8f));
+	}
+
+	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComponent"));
+	SpringArmComponent->SetupAttachment(GetRootComponent());
+	SpringArmComponent->SetRelativeRotation(FRotator(-45.f, 0.f, 0.f));
+	SpringArmComponent->TargetArmLength = 400.f;
+	SpringArmComponent->bEnableCameraLag = true;
+	SpringArmComponent->CameraLagSpeed = 3.0f;
+
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	CameraComponent->SetupAttachment(SpringArmComponent, USpringArmComponent::SocketName);
+	//CameraComponent->SetRelativeLocation(FVector(-300.f, 0.f, 300.f));
+	//CameraComponent->SetRelativeRotation(FRotator(-45.f, 0.f, 0.f));
+
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
+}
+
+// Called when the game starts or when spawned
+void ACollider::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACollider::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ACollider::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &ACollider::MoveForward);
+	PlayerInputComponent->BindAxis(TEXT("MoveRight"), this, &ACollider::MoveRight);
+
+}
+
+void ACollider::MoveForward(float Value) 
+{
+	FVector Forward = GetActorForwardVector();
+	AddMovementInput(Value * Forward);
+}
+void ACollider::MoveRight(float Value) 
+{
+	FVector Forward = GetActorRightVector();
+	AddMovementInput(Value * Forward);
+}
---
+++ Source/BasicActionGame/Public/BasicActionGame_GameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BasicActionGame_GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BASICACTIONGAME_API ABasicActionGame_GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
