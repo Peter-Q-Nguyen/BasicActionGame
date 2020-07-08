d105a3fea08f5067848c9f2a4f3754a6752a959f
Peter Nguyen <spacechannel6@gmail.com>
2020-07-08 16:29:23 +0100

    create blueprint implementable events for door/switch raising and lowering, linked them to a timeline node to call set door/switch location functoins in C++

--- Source/BasicActionGame/Public/FloorSwitch.h
+++ Source/BasicActionGame/Public/FloorSwitch.h
@@ -26,6 +26,14 @@ public:
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Floor Switch")
 	UStaticMeshComponent* Door;
 
+	/** Initial Location for the door */
+	UPROPERTY(BlueprintReadWrite, Category = "Floor Switch");
+	FVector InitialDoorLocation;
+
+	/** Initial Location for the floor switch */
+	UPROPERTY(BlueprintReadWrite, Category = "Floor Switch");
+	FVector InitialSwitchLocation;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
@@ -39,4 +47,21 @@ public:
 	UFUNCTION()
 	void OnOverlapEnd( UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 
+	UFUNCTION(BlueprintImplementableEvent, Category = "Floor Switch")
+	void RaiseDoor();
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Floor Switch")
+	void LowerDoor();
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Floor Switch")
+	void RaiseFloorSwitch();
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Floor Switch")
+	void LowerFloorSwitch();
+
+	UFUNCTION(BlueprintCallable, Category = "Floor Switch")
+	void UpdateDoorLocation(float Z);
+
+	UFUNCTION(BlueprintCallable, Category = "Floor Switch")
+	void UpdateFloorSwitchLocation(float Z);
 };
